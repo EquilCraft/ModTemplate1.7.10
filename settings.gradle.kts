52f13eca318c376dc88bde6fb4e0784be2417ae3
JustCursed <mytojimbyylabu@gmail.com>
2026-05-14 19:34:45 +0300

    this shit needs more refactoring

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,20 @@
-
 pluginManagement {
-    repositories {
-        maven {
-            // RetroFuturaGradle
-            name = "GTNH Maven"
-            url = uri("https://nexus.gtnewhorizons.com/repository/public/")
-            mavenContent {
-                includeGroup("com.gtnewhorizons")
-                includeGroupByRegex("com\\.gtnewhorizons\\..+")
-            }
-        }
-        gradlePluginPortal()
-        mavenCentral()
-        mavenLocal()
-    }
+	repositories {
+		maven {
+			// RetroFuturaGradle
+			name = "GTNH Maven"
+			url = uri("https://nexus.gtnewhorizons.com/repository/public/")
+			mavenContent {
+				includeGroup("com.gtnewhorizons")
+				includeGroupByRegex("com\\.gtnewhorizons\\..+")
+			}
+		}
+		gradlePluginPortal()
+		mavenCentral()
+		mavenLocal()
+	}
 }
 
 plugins {
-    id("com.gtnewhorizons.gtnhsettingsconvention") version("2.0.20")
+	id("com.gtnewhorizons.gtnhsettingsconvention") version("2.0.24")
 }
